10fbd98364b8a48cde1627ea660298b62db011d5
Chris <christopherhoganjr@icloud.com>
2023-04-11 11:22:35 -0700

    user login and register routes work

---
+++ server/controllers/user.controllers.js
@@ -0,0 +1,63 @@
+const User = require("../models/user.model");
+
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+require("dotenv").config();
+
+module.exports = {
+  register: async (req, res) => {
+    const exist = await User.findOne({ email: req.body.email });
+    if (exist) {
+      res
+        .status(400)
+        .json({ error: "email already exists. please login instead." });
+    } else {
+      User.create(req.body).then((user) => {
+        const usertoken = jwt.sign(
+          {
+            id: user._id,
+          },
+          process.env.SECRET_KEY
+        );
+
+        res
+          .cookie("usertoken", usertoken, process.env.SECRET_KEY, {
+            httpOnly: true,
+          })
+          .json({
+            message: "successfully created user",
+            currentUser: { username: user.username, email: user.email },
+          });
+      });
+    }
+  },
+  login: async (req, res) => {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (user === null) {
+      return res.status(400).json({ error: "account does not exist" });
+    }
+    const correctPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+
+    if (!correctPassword) {
+      return res.status(400).json({ error: "invalid email or password" });
+    }
+
+    const usertoken = jwt.sign(
+      {
+        id: user._id,
+      },
+      process.env.SECRET_KEY
+    );
+
+    res
+      .cookie("usertoken", usertoken, process.env.SECRET_KEY, {
+        httpOnly: true,
+      })
+      .json({ name: user.name, email: user.email });
+  },
+};
