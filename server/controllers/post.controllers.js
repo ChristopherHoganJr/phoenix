390eb74ef731bbb6e862f11721f9221a7053ade3
Chris <christopherhoganjr@icloud.com>
2023-04-13 11:36:15 -0700

    like - dislike feature fixed

--- server/controllers/post.controllers.js
+++ server/controllers/post.controllers.js
@@ -29,23 +29,60 @@ module.exports = {
     User.findById(decoded.id)
       .then((user) => {
         Post.findById(req.body._id)
-          .then((post) =>
-            Post.updateOne(
-              { _id: post._id },
-              {
-                author: post.author,
-                text: post.text,
-                like: {
-                  users: [...post.like.users, user._id],
+          .then((post) => {
+            if (post.dislike.users.includes(user._id))
+              post.dislike.users.splice(
+                post.dislike.users.indexOf(user._id),
+                1
+              );
+            if (!post.like.users.includes(user._id))
+              Post.updateOne(
+                { _id: post._id },
+                {
+                  author: post.author,
+                  text: post.text,
+                  like: {
+                    users: [...post.like.users, user._id],
+                  },
+                  dislike: post.dislike,
                 },
-                dislike: post.dislike,
-              },
-              {
-                new: true,
-                runValidators: true,
-              }
-            ).then((updatedPost) => res.status(200).json(updatedPost))
-          )
+                {
+                  new: true,
+                  runValidators: true,
+                }
+              ).then((updatedPost) => res.status(200).json(updatedPost));
+          })
+          .catch((error) =>
+            res.status(400).json({ erros: "error updating like" })
+          );
+      })
+      .catch((error) => res.status(400).json({ errors: "please log in" }));
+  },
+  dislike_post: async (req, res) => {
+    let decoded = jwt.verify(req.cookies.usertoken, process.env.SECRET_KEY);
+    User.findById(decoded.id)
+      .then((user) => {
+        Post.findById(req.body._id)
+          .then((post) => {
+            if (post.like.users.includes(user._id))
+              post.like.users.splice(post.like.users.indexOf(user._id), 1);
+            if (!post.dislike.users.includes(user._id))
+              Post.updateOne(
+                { _id: post._id },
+                {
+                  author: post.author,
+                  text: post.text,
+                  like: post.like,
+                  dislike: {
+                    users: [...post.dislike.users, user._id],
+                  },
+                },
+                {
+                  new: true,
+                  runValidators: true,
+                }
+              ).then((updatedPost) => res.status(200).json(updatedPost));
+          })
           .catch((error) =>
             res.status(400).json({ erros: "error updating like" })
           );
--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -5,5 +5,5 @@ module.exports = (app) => {
   app.post("/api/post", authenticate, Post.new_post);
   app.get("/api/posts", authenticate, Post.all_posts);
   app.put("/api/posts/like", authenticate, Post.like_post);
-  // app.put("/api/posts/dislike", authenticate, Post.dislike_post);
+  app.put("/api/posts/dislike", authenticate, Post.dislike_post);
 };
